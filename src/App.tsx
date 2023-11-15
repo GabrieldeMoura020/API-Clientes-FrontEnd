d0ad9e263153d137b780400f996bb8bdeaab4f48
GABRIEL ROCHADEL DE MOURA <gabriel.moura@alunos.sc.senac.br>
2023-11-14 21:55:05 -0300

    Criando Cadastro Cliente

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api, { IDataRequest, IDataResponse } from './provider/api';
+import { GridColDef } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid/DataGrid';
+import { width } from '@mui/system';
+import { Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [clientes, setClientes] = useState<any>([]);
+
+  const colunas: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID"
+    },
+    {
+      field: "nome",
+      headerName: "Nome"
+    },
+    {
+      field: "sobreNome",
+      headerName: "Sobre Nome"
+    },
+    {
+      field: "dataNascimento",
+      headerName: "Data Nascimento",
+      width: 150
+    },
+    {
+      field: "email",
+      headerName: "E-mail"
+    },
+    {
+    field: "telefone",
+    headerName: "Telefone"
+    }
+  ]
+
+  const carregarClientes = async () => {
+    const request: IDataRequest = {
+      url: "/clientes/"
+    }
+
+    const response: IDataResponse = await api.get(request);
+    if (response.statusCode === 200) {
+      setClientes(response.data)
+    }
+  }
+
+  return(
+    <div>
+      <button onClick={() => {
+        carregarClientes();
+      }}>
+        Carregar
+      </button>
+      <Link to={"/criarCliente"}>Criar Cliente</Link>
+      <div>
+        <DataGrid 
+          rows={clientes}
+          columns={colunas} 
+          checkboxSelection
+          pageSizeOptions={[10,25,50]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize:5
+              }
+            }
+          }}
+          />
+      </div>
     </div>
   );
+
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CriarCliente from './pages/CriarCliente';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/criarCliente",
+    element: <CriarCliente/>
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
